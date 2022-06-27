1846be95bbfd7edc5d414c543f53e99f4a9bc29d
Konstantin Shamshudinov <kakatansky@gmail.com>
2022-06-27 19:19:57 +0300

    BIO information.

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //Name
+    print("Konstantin")
+    //Surname
+    println(" Shamshudinov!")
+    //Place of living
+    print("City -")
+    println(" Moscow!")
+    //Coding skills
+    print("This is my first step in programing.")
+}
