1f7fc171f2215d0f351ace1d4cde5e0b566a1882
Konstantin <k.shamshudinov@gmail.com>
2022-06-28 14:39:03 +0300

    BIO information.

---
+++ src/main/kotlin/homeWork1.kt
@@ -0,0 +1,17 @@
+/*Выведите текст в консоль с помощью функций print и println.
+Воспользуйтесь несколькими вызовами функции print/println.
+Оставьте однострочный или блочный комментарий к вашему коду.
+Закомментируйте одну или несколько строк вашего кода (Для этого необходимо чтобы в вашем коде было несколько вызовов функций print/println.
+Запустите программу, чтобы проверить, что она работает*/
+
+fun main() {
+    //Name
+    print("Konstantin")
+    //Surname
+    println("Shamshudinov.")
+    //Place of living
+    print("City -")
+    println(" Moscow!")
+    //Skills
+    print("This is my first step in programming :)")
+}
